0348c4075b02f64b0e08e1b7075bc55eb6fdd86c
punk4156 <80659633+punk4156@users.noreply.github.com>
2021-07-09 14:31:05 -0400

    navbar positioning and styling

--- packages/nouns-webapp/src/components/NavBar/index.tsx
+++ packages/nouns-webapp/src/components/NavBar/index.tsx
@@ -19,35 +19,25 @@ const NavBar = () => {
       <Navbar.Brand href="#home">
         <img
           src="/logo.svg"
-          width="70"
-          height="70"
+          width="100"
+          height="100"
           className="d-inline-block align-middle"
           alt="React Bootstrap logo"
         />
       </Navbar.Brand>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          {/* <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown> */}
+      <Navbar.Collapse className="justify-content-end">
+        <Nav>
+          <Nav.Link href="#link">Learn</Nav.Link>
+          <Navbar.Text>
+            {activeAccount ? (
+              <ShortAddress>{activeAccount}</ShortAddress>
+            ) : (
+              <Button onClick={() => activateBrowserWallet()}>Connect Wallet</Button>
+            )}
+          </Navbar.Text>
         </Nav>
-        {/* <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form> */}
-        {activeAccount ? (
-          <ShortAddress>{activeAccount}</ShortAddress>
-        ) : (
-          <Button onClick={() => activateBrowserWallet()}>Connect Wallet</Button>
-        )}
       </Navbar.Collapse>
     </Navbar>
     </Container>
