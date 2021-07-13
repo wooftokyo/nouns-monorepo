28648bfc555456281ed0456d68cc6eb04e5c46f4
punk4156 <80659633+punk4156@users.noreply.github.com>
2021-07-13 14:53:36 -0400

    basic layout

--- packages/nouns-webapp/src/components/NounBody/NounBody.tsx
+++ packages/nouns-webapp/src/components/NounBody/NounBody.tsx
@@ -3,15 +3,16 @@ import TopTorso from './TopTorso';
 import Arm from './Arm';
 import Torso from './Torso';
 import classes from './NounBody.module.css';
+import Docs from '../Docs/Docs';
 
 const NounBody = () => {
   return (
-    <Row noGutters={true} className={classes.body}>
-      <Col lg={{ span: 11, offset: 1 }}>
+    <Row noGutters={true}>
+      <Col className={classes.body} lg={{ span: 9, offset: 3 }}>
         <Row>
-          <TopTorso />
-          <Arm />
-          <Torso />
+          <Col lg={{ span: 9, offset: 1}} className='p-5'>
+            <Docs />
+          </Col>
         </Row>
       </Col>
     </Row>
